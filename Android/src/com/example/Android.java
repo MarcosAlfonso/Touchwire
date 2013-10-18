5412c2eb95e12ea997b7719ee3e30b0246dd10ab
Marcos Alfonso <marcostocoast@gmail.com>
2013-10-17 23:22:49 -0500

    Initial Commit

---
+++ Android/src/com/example/Android.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mjm.Touchwire.Main;
+
+public class Android extends AndroidApplication
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
+        cfg.useWakelock = true;
+        cfg.useGL20 = true;
+        initialize(new Main(), cfg);
+    }
+}
---
+++ Main/src/com/mjm/Touchwire/Board.java
@@ -0,0 +1,13 @@
+package com.mjm.Touchwire;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marcos
+ * Date: 10/17/13
+ * Time: 11:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Board
+{
+
+}
