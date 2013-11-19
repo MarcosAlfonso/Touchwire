12e617079828c9989611c5e3ac3773599fece0f9
JoshuaStrunk <joshua.p.strunk@gmail.com>
2013-11-19 11:55:58 -0600

    Updated Zone to full detect and enlarged art for zone
    zone now calculates angle of tangible
    zone now generates a key based on dist between points
    fully commented calculateTangibleType in zone class
    increased size of zone art to better reflect size of tangible object

--- Main/src/com/mjm/Touchwire/Entities/Zone.java
+++ Main/src/com/mjm/Touchwire/Entities/Zone.java
@@ -34,19 +34,49 @@ public class Zone extends Component
             tangibleType = TangibleTypes.NoTangible;
         else
         {
-            int value = 0;// Used to find the top-left corner "should" be lowest value
-            int topLeftTouch;
-            for(int i : touchList)
+            float lValue = 0;// Used to find the top-left point "should" be lowest value
+            float rValue = 0;// Used to find the top-right point "should" be lowest value
+            float componentKey = 0;// Used to figure out what component this is dst of all points added together
+            int topLeftTouch = -1;// "should" become pointer to topLeftTouch if stays -1 serious error has occurred
+            int topRightTouch = -1;// "should" become pointer to topRightTouch if stays -1 serious error has occurred
+
+            //Loop through all the touches on object to calculate top-left point, top-right point and component key
+            for(int i = 0; i < touchList.size(); i++)
             {
-                int touchX = Gdx.input.getX(i);
-                int touchY = Gdx.input.getY(i);
-                if(touchX * touchY < value){
-                    value = touchX * touchY;
-                    topLeftTouch = i;
+                int currentTouch = touchList.get(i);// Main touch being looked at
+                int nextTouch = touchList.get(i+1 % touchList.size());// Next touch (looped) to be looked at
+                Vector2 currentPoint = new Vector2(Gdx.input.getX(currentTouch), Gdx.input.getY(currentTouch));// Vector of current point
+                Vector2 nextPoint = new Vector2(Gdx.input.getX(nextTouch), Gdx.input.getY(nextTouch));// Vector of next point
+
+                //Find the top left point (HACKY)
+                float leftKeyValue = currentPoint.x * currentPoint.y;
+                if(leftKeyValue < lValue){
+                    lValue = leftKeyValue;
+                    topLeftTouch = currentTouch;
                 }
+                //Find the top right point (HACKY)
+                float rightKeyValue = (GameManager.ScreenX - currentPoint.x) * currentPoint.y;
+                if(rightKeyValue < rValue)
+                {
+                    rValue = rightKeyValue;
+                    topRightTouch = currentTouch;
+                }
+                //Calculate the componentKey
+                componentKey += currentPoint.dst(nextPoint);
             }
 
+            //Calculates a Vector to represent x-axis in the local space of the tangible using the top-l and top-r points
+            Vector2 tangibleX = new Vector2(Gdx.input.getX(topRightTouch) - Gdx.input.getX(topLeftTouch), Gdx.input.getY(topRightTouch) - Gdx.input.getY(topLeftTouch));
 
+            //Angle of rotation to the local space of the tangible from the world space
+            float angleOfRot = tangibleX.angle();
+
+            //For button/switch on tangible states
+            if(touchList.size() == 4)
+                componentKey = 9001;
+            //Check for an error state
+            if(touchList.size() >4)
+                componentKey = -1; //error state;
         }
 
 
